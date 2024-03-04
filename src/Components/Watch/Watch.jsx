0d93934dc0b84312afc7af7f22647ac814e7c61e
emdad118661 <emdad118661@gmail.com>
2024-03-04 21:35:19 +0600

    create own fake api in a json file

--- src/Components/Watch/Watch.jsx
+++ src/Components/Watch/Watch.jsx
@@ -1,8 +1,10 @@
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name, price} = watch;
     return (
         <div>
-            
+            <h5>Name: {name}</h5>
+            <p>Price: {price}</p>
         </div>
     );
 };
